8d0303e730190c7a22ca750e9291d25d4d7865a4
Davide Fici <davidefici97@hotmail.it>
2023-04-11 16:08:42 +0200

    add Orders page

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import {Col, Container, Row, Table} from "reactstrap";
 
 const ordersData = [
-  { id: 1, customer: 'John', product: 'box1', quantity: 2, total: 2000 },
-  { id: 2, customer: 'Jane', product: 'box2', quantity: 1, total: 1500 },
-  { id: 3, customer: 'Mike', product: 'box3', quantity: 3, total: 3000 },
+  { id: 1, customer: 'Negozio dolci', product: 'box1', quantity: 2, total: 20 },
+  { id: 2, customer: 'Pizzeria prova', product: 'box2', quantity: 1, total: 15 },
+  { id: 3, customer: 'Pasticceria 2', product: 'box3', quantity: 3, total: 3 },
 ];
 const Orders = () => {
     const [orders, setOrders] = useState(ordersData);
@@ -17,10 +17,10 @@ const Orders = () => {
                 <thead>
                 <tr>
                   <th>ID</th>
-                  <th>Customer</th>
-                  <th>Product</th>
-                  <th>Quantity</th>
-                  <th>Total</th>
+                  <th>Negozio</th>
+                  <th>Prodotto</th>
+                  <th>Quantità</th>
+                  <th>Totale</th>
                 </tr>
                 </thead>
                 <tbody>
